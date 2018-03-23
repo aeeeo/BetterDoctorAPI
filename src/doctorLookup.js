0f1924cf091a0557ba3d22a7f0a7722ed18010be
Andrew Peterson <peterson8000@yahoo.com>
2018-03-23 10:01:18 -0700

    add first working call

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 
 function getDoctors(ailment, displayResult, displayError) {
 
-$.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.5231%2C-122.679%2C100&user_location=45.5231%2C-122.679&skip=0&limit=10&user_key=71ffa702d4c2ebddbf54e7eba21a2d16`).then(function(response){
-
+$.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.5127%2C-122.679%2C100&user_location=45.5231%2C-122.679&skip=0&limit=10&user_key=71ffa702d4c2ebddbf54e7eba21a2d16`).then(function(response){
       let doctorList = [];
-        displayResult(response, newTemp);
+      // $.each(response, function(data.practices.first_name, value){
+        displayResult(response);
+      // })
       }).fail(function(error) {
         displayError(error);
       });
--- src/main.js
+++ src/main.js
@@ -1,21 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import { getDoctor } from "../src/doctorLookup.js";
+import { getDoctors } from "../src/doctorLookup.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
     $('#getAilment').click(function() {
       let ailment = $('#ailment').val();
-      $('#location').val("");
-      getDoctor(ailment, displayResult, displayError);
+      getDoctors(ailment, displayResult, displayError);
   });
 });
 
-let displayResult = function(response, ){
-  $('.showDoctor').text(``);
+let displayResult = function(response){
+  $('.showDoctorList').text(`${response.data[0].practices[0].location_slug}`);
 }
 
 let displayError = function(error){
-  $('.errors').text('');
+  $('.errors').text('error');
 }
