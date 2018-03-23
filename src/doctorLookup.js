143e85e36187c7c1b38ef65c6e2c44c67f98750c
Andrew Peterson <peterson8000@yahoo.com>
2018-03-23 11:10:10 -0700

    change from function to class

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 
-function getDoctors(ailment, displayResult, displayError) {
-
+export class GetDoctors {
+  constructor(ailment, displayResult, displayError) {
+    this.ailment = ailment;
+    this.displayResult = displayResult;
+    this.displayError = displayError;
+  }
+doIt(ailment, displayResult, displayError){
 $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.5127%2C-122.679%2C100&user_location=45.5231%2C-122.679&skip=0&limit=10&user_key=71ffa702d4c2ebddbf54e7eba21a2d16`).then(function(response){
-      let doctorList = [];
-      // $.each(response, function(data.practices.first_name, value){
         displayResult(response);
-      // })
       }).fail(function(error) {
         displayError(error);
       });
 }
-export { getDoctors };
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import { getDoctors } from "../src/doctorLookup.js";
+import { GetDoctors } from "../src/doctorLookup.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
     $('#getAilment').click(function() {
       let ailment = $('#ailment').val();
-      getDoctors(ailment, displayResult, displayError);
+      let getDoctors = new GetDoctors(ailment, displayResult, displayError);
+      getDoctors.doIt(ailment, displayResult, displayError);
   });
 });
 
 let displayResult = function(response){
-  $('.showDoctorList').text(`${response.data[0].practices[0].location_slug}`);
+  $('.showDoctorList').text(`${response.data[0].profile.first_name}`);
 }
 
 let displayError = function(error){
