618745fdd14afab0b968d66154973de0ac2b87ed
Andrew Peterson <peterson8000@yahoo.com>
2018-03-23 13:53:49 -0700

    add working div creation

--- src/displayResult.js
+++ src/displayResult.js
@@ -1,11 +1,17 @@
 import $ from 'jquery';
 
 function displayResult(response) {
+  let doctorArray = []
     $.each(response.data, function(i){
-      $.each(response.data[i].profile, function(key, value){
-      console.log(key + " " + value);
-      });
+      doctorArray.push(response.data[i].profile);
     });
+    console.log(doctorArray[0].slug);
+
+    for (var i = 0; i < doctorArray.length; i++) {
+      $('.showDoctor').append($('<div/>', { id: 'showDoctor' + i }))
+      $('#showDoctor'+i).html('<img src="' + doctorArray[i].image_url + '">');
+      console.log(doctorArray[i].image_url)
+    }
     $('.showDoctorName').text(`${response.data[0].profile.first_name} ${response.data[0].profile.last_name}`);
     $('.showDoctorURL').html(`<img src="">`);
   }
